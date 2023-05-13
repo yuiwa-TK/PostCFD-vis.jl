1a275b5fba56c2de83678e906b429c4e3d005357
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2023-05-13 22:09:09 +0900

    testcode

--- src/PostCFDvis.jl
+++ src/PostCFDvis.jl
@@ -4,10 +4,37 @@ using PlotlyJS
 using PostCFD
 # using Reexport
 
-# Write your package code here.
-function VisuApp()
-    pwd()
+
+function greet()
+    com=`echo -e "\e[36;1m================================\e[m"`;
+    com1= `echo -e "\e[36;1mRunning PostCFDvis...\e[m"`;
+    run(com);run(com1);run(com);
+    print("current directory::");pwd();
+end
+
+function julia_main()
+    greet();
+    try 
+        ExecPlotApp()
+    catch
+        Base.invokelatest(Base.display_error,Base.catch_stack())
+        return 1
+    end
     # include("../test/dataset/test_vis.jl")
+    return 0
+end
+
+function ExecPlotApp()
+    @show ARGS
+    @show Base.PROGRAM_FILE
+    # println("Parsing input arguments...")
+    # Dataparams=parase
+    @show readlines()
+    # Datasets = DataFormatting(Data)
+
+    # Main_plot(Datasets, Plotparams)
+
+    return 0
 end
 
 
